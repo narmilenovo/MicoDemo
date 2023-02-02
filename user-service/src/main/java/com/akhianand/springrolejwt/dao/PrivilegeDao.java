bb81620e836732c199cd0024b2e753f2daa3177c
H. Mohamed Imran <107101737+narmilenovo@users.noreply.github.com>
2023-02-02 16:58:04 +0530

    Delete user-service directory

--- user-service/src/main/java/com/akhianand/springrolejwt/dao/PrivilegeDao.java
+++
@@ -1,9 +0,0 @@
-package com.akhianand.springrolejwt.dao;
-
-import com.akhianand.springrolejwt.model.Privilege;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrivilegeDao extends JpaRepository<Privilege, Long> {
-
-    Privilege findPrivilegeByName(String name);
-}
